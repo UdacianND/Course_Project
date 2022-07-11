34f34669c16fa8c02a9c18f7dfb526c03b922deb
IbnHamid <ibnhamid777@gmail.com>
2022-07-11 15:48:14 +0500

    a

--- src/main/java/course_project/security/CORSFilter.java
+++ src/main/java/course_project/security/CORSFilter.java
@@ -11,33 +11,17 @@ import java.util.List;
 @Configuration
 public class CORSFilter implements Filter {
 
-    // This is to be replaced with a list of domains allowed to access the server
-  //You can include more than one origin here
     private final List<String> allowedOrigins = List.of("https://itransition-course-work.herokuapp.com");
 
-    public void destroy() {
-
-    }
-
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
-        // Lets make sure that we are working with HTTP (that is, against HttpServletRequest and HttpServletResponse objects)
         if (req instanceof HttpServletRequest request && res instanceof HttpServletResponse response) {
 
-            // Access-Control-Allow-Origin
             String origin = request.getHeader("Origin");
             response.setHeader("Access-Control-Allow-Origin", allowedOrigins.contains(origin) ? origin : "");
             response.setHeader("Vary", "Origin");
-
-            // Access-Control-Max-Age
-            response.setHeader("Access-Control-Max-Age", "3600");
-
-            // Access-Control-Allow-Credentials
+            response.setHeader("Access-Control-Max-Age", "36000");
             response.setHeader("Access-Control-Allow-Credentials", "true");
-
-            // Access-Control-Allow-Methods
             response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
-
-            // Access-Control-Allow-Headers
             response.setHeader("Access-Control-Allow-Headers",
                 "Origin, X-Requested-With, Content-Type, Accept, " + "X-CSRF-TOKEN");
         }
